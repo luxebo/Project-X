d993f2d39916825baf7d7452e46d94d82e0d92d4
luxebo <34826088+luxebo@users.noreply.github.com>
2018-06-17 16:19:55 -0700

    Add files via upload

---
+++ MainMenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void ChangeScript(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
